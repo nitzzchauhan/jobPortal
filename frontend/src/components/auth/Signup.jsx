498a979c13a8eb915f87a63e028e4b115b1fdff5
nitin chauhan <nitinkiranchauhan@gmail.com>
2024-12-05 10:18:11 +0530

    backend

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -68,7 +68,7 @@ const Signup = () => {
         }
     },[])
     return (
-        <div>
+        <div> 
             <Navbar />
             <div className='flex items-center justify-center max-w-7xl mx-auto'>
                 <form onSubmit={submitHandler} className='w-1/2 border border-gray-200 rounded-md p-4 my-10'>
